38c6911b1fea459ca609eaa899cc7593827bd594
Richard Opiyo <romenyo@dewcis.com>
2021-11-18 10:24:20 +0300

    fixed the issue with signing up

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ app.get("/",function(req,res){
 })
 
 // API KEY -  668ade3e7d45d1a0d0c855e05dbbe94d-us20
+// nwe api key - aed2cc76f2b330fa0a04b5e3bf086a56-us20
 // List id -  3d9c56cfd9.
+
 app.post("/",function(req,res){
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
@@ -25,27 +27,34 @@ app.post("/",function(req,res){
   const url = "https://us20.api.mailchimp.com/3.0/lists/3d9c56cfd9";
   const options = {
     method: "POST",
-    auth: "richie : 668ade3e7d45d1a0d0c855e05dbbe94d-us20"
+    auth: "richie:"
   }
   const data = {
-    members:[
-            {
+    members:
+            [{
               email_address: email,
-              // status: subscribed,
-              merge_fields: [
-                              {
+              status: "subscribed",
+              merge_fields:
+                            {
                               FNAME: firstName,
-                              LNAME: lastName
-                              }
-                            ]
-            }
-            ]
-  };
+                              LNAME: lastName,
+                              BIRTHDAY: "01/22",
+                              ADDRESS: {
+                                          addr1: "123 Freddie Ave",
+                                          city: "Atlanta",
+                                          state: "GA",
+                                          zip: "12345",
+                                      },
+            },
+
+  }],
+};
 
   const jsonData = JSON.stringify(data);
 
-  const request = https.get(url,options,function(response){
+  var request = https.request(url,options,function(response){
     response.on("data",function(data){
+      console.log("passed");
       console.log(JSON.parse(data));
     });
   })
@@ -53,9 +62,9 @@ app.post("/",function(req,res){
   request.write(jsonData);
   request.end();
 
-})
+});
 
 
 app.listen(3000,function(){
   console.log("Connected on Port 3000");
-})
+});
